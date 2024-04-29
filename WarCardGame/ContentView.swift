14f2277475e6355275fc40173c60a3fb455fb462
Sadia Tasnim <tasnimsadia259@gmail.com>
2024-04-29 09:41:29 +0600

    Initial Commit

---
+++ WarCardGame/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WarCardGame
+//
+//  Created by Sadia on 29/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Sadia on 29/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
